4bd9be4e37644c3055d4feebf7c206cd5015493d
frank113 <frankkovacs8@gmail.com>
2026-05-29 19:26:48 -0400

    Update build scripts

--- models/ollama/qwen3.5-9b/build.sh
+++ models/ollama/qwen3.5-9b/build.sh
@@ -6,7 +6,5 @@ echo "Making qwen3.5-9b Tuned models"
 
 ollama create qwen35-9b:ctx4k  -f Modelfile.4k
 ollama create qwen35-9b:ctx8k  -f Modelfile.8k
-ollama create qwen35-9b:ctx16k -f Modelfile.16k
-ollama create qwen35-9b:ctx32k -f Modelfile.32k
 
 ## ollama list | grep qwen35
